108d33b5815ef6f6311a3e6c99e23199c5aa3d0f
Llewellyn Roos <llewellyn.roos@gmail.com>
2023-05-14 17:21:48 +0200

    add neopixel light control

--- main.ts
+++ main.ts
@@ -1,30 +1,45 @@
 radio.onReceivedValue(function (name, value) {
     if (name == "x") {
-        sprite.change(LedSpriteProperty.X, value)
+        strip.clear()
         left = 0 - value
         right = value
+        if (right > 0) {
+            strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Orange))
+        } else if (left > 0) {
+            strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Orange))
+        } else {
+        	
+        }
+        strip.show()
     } else if (name == "y") {
-        sprite.change(LedSpriteProperty.Y, value)
         speed = value
+        basic.showIcon(IconNames.Happy, 0)
     } else if (name == "button_c") {
-        music.playMelody("G G G C5 C5 - E E ", 500)
-        music.playMelody("C5 - C5 B B A A G ", 404)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 924, 894, 255, 101, 100, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 924, 894, 255, 101, 100, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
     } else if (name == "button_d") {
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 1600, 1, 255, 92, 300, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 400, 600, 255, 0, 150, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
     } else if (name == "button_e") {
-    	
+        basic.showIcon(IconNames.Skull, 0)
+strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 1600, 1, 255, 92, 300, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+        strip.clear()
+        strip.show()
     } else {
-    	
+        basic.showIcon(IconNames.Heart, 0)
+strip.showRainbow(1, 360)
     }
     wuKong.setAllMotor(speed * 30 + left * 20, speed * 30 + right * 20)
 })
+let strip: neopixel.Strip = null
 let right = 0
 let left = 0
 let speed = 0
-let sprite: game.LedSprite = null
-sprite = game.createSprite(2, 2)
 speed = 0
 left = 0
 right = 0
 wuKong.stopAllMotor()
 wuKong.setLightMode(wuKong.LightMode.BREATH)
+strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+basic.showIcon(IconNames.Happy)
